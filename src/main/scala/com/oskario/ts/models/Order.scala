003c994fdc049afc07d1141efeff13d10349de17
Oskar <oskario2007@gmail.com>
2014-07-06 17:32:50 +0200

    Withdrawing from order added

--- src/main/scala/com/oskario/ts/models/Order.scala
+++ src/main/scala/com/oskario/ts/models/Order.scala
@@ -1,3 +1,7 @@
 package com.oskario.ts.models
 
-case class Order(instrument: String, price: BigDecimal, volume: BigDecimal) extends OrderLike(price, volume)
+case class Order(instrument: String, price: BigDecimal, volume: BigDecimal) extends OrderLike(price, volume) {
+  def withdraw(value: BigDecimal): Order = {
+    Order(instrument, price, volume - value)
+  }
+}
